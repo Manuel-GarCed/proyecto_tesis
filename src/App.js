bbf99258699dcc877a4daed4a49c95f4bc0f6f71
Manuel-GarCed <manuelgarciacedeno539@gmail.com>
2025-06-14 16:46:26 -0500

    cambios antes de Vite

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
+import React, { useState } from 'react';
+
+//Añadiendo contador de prueba
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>Contador: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Header />
+        {/* más componentes */}
+      </div>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,6 +36,7 @@ function App() {
         >
           Learn React
         </a>
+        <Counter />
       </header>
     </div>
   );
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <h1>Bienvenido al sistema de Tesis</h1>;
+}
+
+export default Header;
